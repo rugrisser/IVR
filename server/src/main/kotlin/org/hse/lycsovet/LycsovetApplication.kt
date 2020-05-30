5c190791a5e9f42d237ee6e278b6c0f9b808c8b2
rugrisser <rugrisser@gmail.com>
2020-05-30 21:32:46 +0300

    Server Initialization

---
+++ server/src/main/kotlin/org/hse/lycsovet/LycsovetApplication.kt
@@ -0,0 +1,11 @@
+package org.hse.lycsovet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LycsovetApplication
+
+fun main(args: Array<String>) {
+    runApplication<LycsovetApplication>(*args)
+}
---
+++ server/src/test/kotlin/org/hse/lycsovet/LycsovetApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.hse.lycsovet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LycsovetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
